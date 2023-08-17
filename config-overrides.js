7e710526307db4c47eaa230e538b7ad0f6323052
fanchunbo <fanbobo1996@gmial.com>
2023-08-17 14:20:53 +0800

    :green_heart: fix: 优化打包速度和占用

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,36 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const HappyPack = require("happypack");
+let happyThreadPool = HappyPack.ThreadPool({ size: 8 });
 module.exports = function override(config, env) {
-
 	config.resolve.fallback = {
-		url: require.resolve('url'),
-		assert: require.resolve('assert'),
-		crypto: require.resolve('crypto-browserify'),
-		http: require.resolve('stream-http'),
-		https: require.resolve('https-browserify'),
-		os: require.resolve('os-browserify/browser'),
-		buffer: require.resolve('buffer'),
-		stream: require.resolve('stream-browserify'),
+		url: require.resolve("url"),
+		assert: require.resolve("assert"),
+		crypto: require.resolve("crypto-browserify"),
+		http: require.resolve("stream-http"),
+		https: require.resolve("https-browserify"),
+		os: require.resolve("os-browserify/browser"),
+		buffer: require.resolve("buffer"),
+		stream: require.resolve("stream-browserify"),
 	};
 	config.plugins.push(
 		new webpack.ProvidePlugin({
-			process: 'process/browser',
-			Buffer: ['buffer', 'Buffer'],
+			process: "process/browser",
+			Buffer: ["buffer", "Buffer"],
 		}),
+		new HappyPack({
+			id: "js",
+			threadPool: happyThreadPool,
+			loaders: ["babel-loader"],
+		}),
+
+		new HappyPack({
+			id: "styles",
+			threadPool: happyThreadPool,
+			loaders: ["style-loader", "css-loader", "sass-loader"],
+		})
+		// new BundleAnalyzerPlugin()
 	);
 	config.ignoreWarnings = [/Failed to parse source map/];
 	return config;
-}
+};
